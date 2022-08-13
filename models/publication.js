a829a5dea235b5c7a7455e6e7ca65bb0891f15a0
Olajumoke <rukkylaw.tl@gmail.com>
2022-08-13 19:20:51 +0100

    adding categories and user's address to the publication schema

--- models/publication.js
+++ models/publication.js
@@ -10,25 +10,35 @@ const PublicationSchema = new mongoose.Schema(
       required: [true, "Kindly enter a short description of the publication "],
     },
     cid: {
-        type: String,
-        required: [true, "Kindly pass the application CID"]
+      type: String,
+      required: [true, "Kindly pass the publication CID"],
     },
-    category: {
-        type: String,
-        enums: {
-            values: ['premium', 'free'],
-            message: '{VALUE} is not supported'
-        },
-        default: 'free'
+    categories: {
+      type: Array,
+      of: String,
+      required: [true, "Kindly pass the publication's categories"],
+    },
+    type: {
+      type: String,
+      enums: {
+        values: ["premium", "free"],
+        message: "{VALUE} is not supported",
+      },
+      default: "free",
     },
     views: {
-        type: Number,
-        default: 0
+      type: Number,
+      default: 0,
     },
     createdBy: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: [true, 'Kindly provide the user']
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Kindly provide the user"],
+    },
+    userAddress: {
+      type: String,
+      required: [true, "Kindly provide user's address"],
+      match: [/^0x[a-fA-F0-9]{40}$/, "Kindly provide a valid wallet address"],
     },
   },
   { timestamps: true }
