2ff19e400de315c261bec56a2802bf4007e372c2
Olajumoke <rukkylaw.tl@gmail.com>
2022-08-13 17:56:27 +0100

    sending userId with token on signing

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,7 +5,7 @@ const signUser = async (req, res) => {
     const { address } = req.body
     let user = await User.findOne({ address });
     if (!user) user = await User.create({ address });
-    return res.status(StatusCodes.CREATED).json({ token: user.getToken() })
+    return res.status(StatusCodes.CREATED).json({ token: user.getToken(), UserId: user._id })
 }
 
 
