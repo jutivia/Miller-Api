4b636e12690f1dec383475ced1704a75132f9e86
Olajumoke <rukkylaw.tl@gmail.com>
2022-08-13 01:13:23 +0100

    adding heroku

--- app.js
+++ app.js
@@ -9,8 +9,27 @@ const publicationRoute = require('./routes/publication')
 const notFound = require('./middleware/not-found')
 const errorHandler = require("./middleware/error-handler");
 const auth = require("./middleware/authentication");
+//security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 
 //middlewares
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+
 app.use(express.json())
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/publications", [auth, publicationRoute])
@@ -19,7 +38,9 @@ app.use(notFound, errorHandler);
 
 
 
-
+app.get('/', (req, res) => {
+    res.send('Jobs api')
+})
 
 
 
